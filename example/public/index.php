b2d97ef2840b5faaf6f69b44f5af325fd19358d1
Juliano Petronetto <juliano.petronetto@gmail.com>
2018-02-04 18:19:56 -0200

    Added a Postgres database example

--- example/public/index.php
+++ example/public/index.php
@@ -1,1 +1,16 @@
-<?php phpinfo();
+<?php
+
+$dbname   = getenv('POSTGRES_DB');
+$user     = getenv('POSTGRES_USER');
+$password = getenv('POSTGRES_PASSWORD');
+$strConn  = "pgsql:host=database;dbname=$dbname;user=$user;password=$password";
+
+try {
+    $conn = new PDO($strConn);
+    echo "<p>Database Connection Sucessful!!!</p>";
+} catch (PDOException $error) {
+    echo "Error:" . $error->getMessage();
+    die();
+}
+
+echo "<p>See <a href='http://{$_SERVER[HTTP_HOST]}/info.php'> Here</a> for PHP info.</p>";
